cc0512f10370cf5ff0161e433858eff5c4a858f4
Abr <59525997+Abr009@users.noreply.github.com>
2024-03-16 13:41:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
 var NewTask = document.getElementById("task");
 var TaskContainer = document.getElementById("tasks");
+var idCounter = 0;
 
-function AddTask(){
-    if(NewTask.value === ''){
+function AddTask() {
+    if (NewTask.value === '') {
         alert("Task can't be empty");
-    }
-    else{
+    } else {
         let li = document.createElement("li");
+        let box = document.createElement("div");
+        box.className = "toggle";
+        box.id = "toggle_" + idCounter++;
+        let label = document.createElement("label");
         let span = document.createElement("span");
 
-        li.innerHTML = NewTask.value;
+        label.appendChild(box);
+        label.appendChild(document.createTextNode(NewTask.value));
         span.innerHTML = "\u00d7";
 
-        TaskContainer.appendChild(li);
+        li.appendChild(label);
         li.appendChild(span);
+
+        TaskContainer.appendChild(li);
     }
     NewTask.value = "";
     saveData();
 }
 
-TaskContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        
+TaskContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LABEL") {
+        toggleBackground(e.target);
+        saveData();
     }
 
-    if(e.target.tagName === "SPAN"){
+    if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
-},false);
+}, false);
 
-function saveData(){
+function toggleBackground(label) {
+    let div = label.querySelector(".toggle");
+
+    if (div.style.backgroundColor === "lightgreen") {
+        div.style.backgroundColor = "red";
+    } else {
+        div.style.backgroundColor = "lightgreen";
+    }
+}
+
+function saveData() {
     localStorage.setItem("data", TaskContainer.innerHTML);
 }
 
-function LoadData(){
+function LoadData() {
     TaskContainer.innerHTML = localStorage.getItem("data");
 }
 
